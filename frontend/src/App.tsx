f402a8f274cb6b446850907505498a6c641120f5
jonas747 <jonasr747@gmail.com>
2021-10-20 21:04:57 +0200

    add placeholder frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>BotLoader coming soon™</p>
+        <img src="/logo192.png" alt="zzz"></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
