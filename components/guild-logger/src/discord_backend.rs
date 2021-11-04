301f1e7db38a33035e2453b189d20fb7398b492f
jonas747 <jonasr747@gmail.com>
2021-11-04 23:01:37 +0100

    new structured guild logging system
    This system is a bit more structured.
    It still needs to be integrated into the bot.

---
+++ components/guild-logger/src/discord_backend.rs
@@ -0,0 +1,58 @@
+use crate::{LogEntry, LogLevel};
+use stores::config::ConfigStore;
+use tracing::error;
+
+pub struct DiscordLogger<CT> {
+    discord_client: twilight_http::Client,
+    config_store: CT,
+}
+
+#[async_trait::async_trait]
+impl<CT> crate::GuildLoggerBackend for DiscordLogger<CT>
+where
+    CT: ConfigStore + Send,
+    CT::Error: 'static,
+{
+    async fn handle_entry(&self, entry: LogEntry) {
+        let guild_id = entry.guild_id;
+        let message = match format_entry(entry) {
+            Some(msg) => msg,
+            None => return,
+        };
+
+        let conf = match self
+            .config_store
+            .get_guild_meta_config_or_default(guild_id)
+            .await
+        {
+            Ok(v) => v,
+            Err(err) => {
+                error!(%err, "failed fetching config for guild logging");
+                return;
+            }
+        };
+
+        if let Some(channel_id) = conf.error_channel_id {
+            if let Ok(next) = self
+                .discord_client
+                .create_message(channel_id)
+                .content(&message)
+            {
+                next.exec().await.ok();
+            }
+        }
+    }
+}
+
+fn format_entry(entry: LogEntry) -> Option<String> {
+    if matches!(entry.level, LogLevel::Error | LogLevel::Critical) {
+        let prefix = if let Some(script_ctx) = entry.script_context {
+            format!("[{} {}]", entry.level, script_ctx)
+        } else {
+            format!("[{}]", entry.level)
+        };
+        Some(format!("{}: {}", prefix, entry.message))
+    } else {
+        None
+    }
+}
---
+++ components/guild-logger/src/lib.rs
@@ -0,0 +1,83 @@
+use std::sync::Arc;
+use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
+
+pub mod discord_backend;
+pub mod entry;
+pub mod guild_subscriber_backend;
+
+pub use entry::{LogEntry, LogLevel, ScriptContext};
+
+#[async_trait::async_trait]
+pub trait GuildLoggerBackend {
+    async fn handle_entry(&self, entry: LogEntry);
+}
+
+struct LoggerTask {
+    backends: Vec<Arc<dyn GuildLoggerBackend + Send + Sync>>,
+    rx: UnboundedReceiver<LogEntry>,
+}
+
+impl LoggerTask {
+    async fn run(&mut self) {
+        loop {
+            while let Some(next) = self.rx.recv().await {
+                self.handle_next(next).await;
+            }
+        }
+    }
+
+    async fn handle_next(&self, entry: LogEntry) {
+        for backend in &self.backends {
+            backend.handle_entry(entry.clone()).await;
+        }
+    }
+}
+
+pub struct GuildLoggerBuilder {
+    backends: Vec<Arc<dyn GuildLoggerBackend + Send + Sync>>,
+}
+
+impl Default for GuildLoggerBuilder {
+    fn default() -> Self {
+        Self {
+            backends: Vec::new(),
+        }
+    }
+}
+
+impl GuildLoggerBuilder {
+    pub fn new() -> Self {
+        Self {
+            backends: Vec::new(),
+        }
+    }
+
+    pub fn add_backend<T: GuildLoggerBackend + Send + Sync + 'static>(&mut self, backend: Arc<T>) {
+        self.backends.push(backend);
+    }
+
+    pub fn run(self) -> GuildLogger {
+        let (tx, rx) = unbounded_channel();
+
+        tokio::spawn(async move {
+            let mut logger_task = LoggerTask {
+                backends: self.backends,
+                rx,
+            };
+
+            logger_task.run().await
+        });
+
+        GuildLogger { tx }
+    }
+}
+
+pub struct GuildLogger {
+    tx: UnboundedSender<LogEntry>,
+}
+
+impl GuildLogger {
+    pub fn log(&self, entry: LogEntry) {
+        self.tx.send(entry).ok();
+    }
+}
