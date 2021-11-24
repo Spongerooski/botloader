a1e5eb18e7fd0a232daa536748f2ae8d58b66341
jonas747 <jonasr747@gmail.com>
2021-11-24 18:15:11 +0100

    gen typings

--- typings/events.d.ts
+++ typings/events.d.ts
@@ -2,6 +2,9 @@ import { CommandInteraction, Message, MessageDelete, MessageUpdate } from './mod
 export declare type EventType = "BOTLOADER_COMMAND_INTERACTION_CREATE" | "MESSAGE_CREATE" | "MESSAGE_UPDATE" | "MESSAGE_DELETE";
 export declare type EventListenerFunction<T> = (a: T) => void;
 export declare type EventDataType<T extends EventType> = T extends "BOTLOADER_COMMAND_INTERACTION_CREATE" ? CommandInteraction : T extends "MESSAGE_CREATE" ? Message : T extends "MESSAGE_UPDATE" ? MessageUpdate : T extends "MESSAGE_DELETE" ? MessageDelete : never;
+/**
+ * @internal
+ */
 export declare class ScriptEventMuxer {
     listeners: EventListener[];
     handleEvent(evt: DispatchEvent): void;
@@ -10,6 +13,9 @@ interface EventListener {
     f: (arg: any) => void;
     event: EventType;
 }
+/**
+ * @internal
+ */
 export declare namespace InternalEventSystem {
     function registerEventMuxer(muxer: ScriptEventMuxer): void;
     function dispatchEvent(evt: DispatchEvent): void;
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,6 @@
 export * from './core_util';
+export * from './commands';
+export * from './events';
 export * from './script_globals';
 export * from './models/index';
+export * from './script';
