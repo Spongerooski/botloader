9012bb866b2df76dab709d9aeef2668417979738
jonas747 <jonasr747@gmail.com>
2021-11-25 23:56:37 +0100

    make a top level type build script

--- components/runtime/src/ts/typedecls.sh
+++ components/runtime/src/ts/typedecls.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set -e
+
+# cd to the script location
+cd "${0%/*}"
+
 tsc --build tsconfig.json
 cp lib.botloader_user.core.d.ts ../../../../typings
 cp script_globals.d.ts ../../../../typings
---
+++ full-gen-types.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# cd to the script location
+cd "${0%/*}"
+
+./components/runtime-models/gen-move-types.bash
+./components/runtime/src/ts/typedecls.sh
